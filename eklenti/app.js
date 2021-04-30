b3b9860d4b31d57fe0a57519a98756bd4b4ce512
berk gümüş <63772343+berk420@users.noreply.github.com>
2021-04-30 17:15:14 +0300

    Add files via upload

---
+++ eklenti/app.js
@@ -0,0 +1,10 @@
+<script>
+            // Here the value is stored in new variable x
+
+            function myFunction() {
+                var x =
+                    document.getElementById("myText").value;
+
+                document.getElementById(
+                  "demo").innerHTML = x;
+</script>
