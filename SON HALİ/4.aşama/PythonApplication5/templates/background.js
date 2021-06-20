4348196d9d451d872cffabf27655cb201ab217ae
berk gümüş <63772343+berk420@users.noreply.github.com>
2021-06-20 15:18:32 +0300

    Add files via upload

---
+++ SON HALİ/4.aşama/PythonApplication5/templates/background.js
@@ -0,0 +1,26 @@
+/*
+window.addEventListener('load',()=>{
+	if (!window.Notification) return;
+
+})
+
+
+const sendNotification =(permission)=>{
+	let notification =new Notification ('yeni bild',{
+		body:'ilOKFHGAŞKJFVNAŞKDFJNBAŞKDJFNVAGDKFVNADŞFKVJNADKFŞJVANDKFJVBN',
+		icon:'icon.png'
+	})
+    notification.onclick =() =>{
+    	window.location.href="https://www.google.com/"
+    }
+    console.log(notification);
+}
+
+	
+const button =document.getElementById('button')
+button.addEventListener('click',()=>{
+	Notification
+	.requestPermission()
+	.then(sendNotification)
+})
+*/
